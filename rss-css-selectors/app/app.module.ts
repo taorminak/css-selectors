af71fa7b5372e7b2f391e05b90d0dd86c225e0d0
Anna Ostanina <ostanina.anna@yahoo.com>
2023-07-01 17:28:28 +0200

    fix: fix levels model

--- rss-css-selectors/app/app.module.ts
+++ rss-css-selectors/app/app.module.ts
@@ -6,7 +6,7 @@ import {
 import Input from './components/Block2: Input window/input';
 import Markup from './components/Block3: Code block/code';
 import LevelsList from './components/Block4: Levels list/levelsList';
-import { levels } from './models/LevelModel';
+import Level, { levels } from './models/LevelModel';
 
 const level1 = levels[0];
 const block1: Layout = createLayout(level1.layout);
@@ -14,18 +14,20 @@ const block1: Layout = createLayout(level1.layout);
 const appContainer: Layout = block1;
 export default appContainer;
 
-const userInput = new Input();
 const block2 = document.getElementById('block2');
-const newElement = document.createElement('input');
 
-newElement.setAttribute('placeholder', 'Type a CSS selector here');
-newElement.setAttribute('type', 'text');
-newElement.appendChild(userInput.input);
+const userInput = new Input();
+
+userInput.input.setAttribute('placeholder', 'Type a CSS selector here');
+userInput.input.setAttribute('type', 'text');
+
+console.log(userInput);
 
 const cssView = document.createElement('div');
 
 cssView.classList.add('editor-window');
 block2?.appendChild(cssView);
+cssView.appendChild(userInput.input);
 
 const buttonSend = document.createElement('div');
 
@@ -33,28 +35,6 @@ buttonSend.classList.add('button-send');
 buttonSend.textContent = 'send';
 
 let currentLevelIndex = 0;
-
-function handleButtonClick(): void {
-  const selector = userInput.getCode();
-
-  const level = levels[currentLevelIndex];
-
-  if (level.correctSelector === selector) {
-    if (currentLevelIndex < levels.length - 1) {
-      currentLevelIndex = +1;
-
-      const nextLevel = levels[currentLevelIndex];
-
-      console.log(nextLevel);
-    } else {
-      console.log('Congratulations! You have completed all levels.');
-    }
-  } else {
-    console.error('Invalid selector');
-  }
-}
-
-buttonSend.addEventListener('click', handleButtonClick);
 
 const styles = document.createElement('div');
 
@@ -65,7 +45,7 @@ const chooseLevel = document.createElement('div');
 
 chooseLevel.classList.add('choose-level');
 chooseLevel.textContent = ' /* Type a number to skip to a level */ ';
-cssView.appendChild(newElement);
+
 cssView.appendChild(buttonSend);
 cssView.appendChild(styles);
 cssView.appendChild(chooseLevel);
@@ -84,3 +64,63 @@ markup.generate(level1);
 export const levelsList = new LevelsList('block4', 'Select elements by their type', 'Type Selector. Selects all elements of type A. Type refers to the type of tag, so div, p and ul are all different element types.');
 
 levelsList.createRulesWindow(level1);
+
+function updateBlock1(level: Level):void {
+  console.log(level.layout);
+
+  const newBlock1: Layout = createLayout(level.layout);
+
+  if (block1) {
+    block1.element.innerHTML = newBlock1.element.innerHTML || '';
+  }
+}
+
+function updateBlock3(level: Level): void {
+  htmlViewer.innerHTML = '';
+  markup.generate(level);
+}
+
+function updateBlock4(level: Level): void {
+  const rules = document.getElementsByClassName('levels')[0] as HTMLElement;
+
+  if (rules) {
+    rules.remove();
+  }
+  levelsList.createRulesWindow(level);
+}
+
+function updateBlocks(level: Level): void {
+  updateBlock1(level);
+  updateBlock3(level);
+  updateBlock4(level);
+}
+
+const victoryMessage = document.createElement('div');
+
+victoryMessage.textContent = 'Congratulations! You have completed all levels.';
+victoryMessage.style.display = 'none';
+victoryMessage.classList.add('victory');
+
+function handleButtonClick(): void {
+  const selector = userInput.getCode();
+
+  const level = levels[currentLevelIndex];
+
+  if (level.correctSelector === selector) {
+    if (currentLevelIndex < levels.length - 1) {
+      currentLevelIndex += 1;
+
+      const nextLevel = levels[currentLevelIndex];
+
+      updateBlocks(nextLevel);
+      userInput.input.value = '';
+    } else {
+      victoryMessage.style.display = 'block';
+    }
+  } else {
+    console.error('Invalid selector');
+  }
+}
+cssView.appendChild(victoryMessage);
+
+buttonSend.addEventListener('click', handleButtonClick);
