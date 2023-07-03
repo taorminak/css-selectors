5157d4dbcd0c7d69db1fc5d67afacca1f0f47eeb
Anna Ostanina <ostanina.anna@yahoo.com>
2023-07-03 18:43:59 +0200

    fix: add display window for highlighting

--- rss-css-selectors/app/components/Highlighter/highlighter.ts
+++ rss-css-selectors/app/components/Highlighter/highlighter.ts
@@ -1,56 +1,76 @@
-import hljs from "highlight.js/lib/core";
-import javascript from "highlight.js/lib/languages/javascript";
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
 
-import Level, { levels } from "../../../models/LevelModel";
-import "highlight.js/styles/github.css";
-import "./highlighter.css";
+import Level, { levels } from '../../../models/LevelModel';
+import 'highlight.js/styles/github.css';
+import './highlighter.css';
 
-hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage('javascript', javascript);
 
 export default function highlightElement(
   targetElement: HTMLElement,
   level: Level,
-  language: string
+  language: string,
 ): void {
   if (targetElement) {
-    targetElement.classList.add("highlight");
+    targetElement.classList.add('highlight');
     setTimeout(() => {
-        targetElement.classList.remove("zoomed");
-      }, 1000);
+      targetElement.classList.remove('zoomed');
+    }, 1000);
 
     const codeString = level.htmlCode;
-    const elements = codeString.split(`<br>`);
+    const elements = codeString.split('<br>');
 
     const targetString = targetElement.innerHTML;
     const index = elements.indexOf(targetString);
-    console.log(index)
+
+    console.log(index);
 
     const comparedLayout = level.layout;
 
     const layoutElements = comparedLayout
       .split(/<|>/)
-      .filter((element) => element.trim() !== "");
+      .filter((element) => element.trim() !== '');
 
-    const highlightElement =  layoutElements[index];
+    const highlightElement = layoutElements[index];
 
-    const island = document.querySelector(".island");
-    const elementsToHighlight = island?.querySelectorAll("*");
-console.log(elementsToHighlight)
+    console.log(highlightElement);
+
+    const island = document.querySelector('.island');
+    const elementsToHighlight = island?.querySelectorAll('*');
+
+    console.log(elementsToHighlight);
+
     if (elementsToHighlight) {
       for (let i = 0; i < elementsToHighlight.length; i++) {
-        if (index > 0 && index <= elementsToHighlight.length) {
-            const element = elementsToHighlight[index-1] as HTMLElement;
-          element.classList.add("zoomed");
+        if (elementsToHighlight && index > 0 && index <= elementsToHighlight.length) {
+          const element = elementsToHighlight[index - 1] as HTMLElement;
+
+          element.classList.add('zoomed');
+
           setTimeout(() => {
-            element.classList.remove("zoomed");
+            element.classList.remove('zoomed');
+            codeDisplay.remove();
           }, 1000);
           // hljs.highlightElement(element);
         }
+
       }
+      const codeDisplay = document.createElement('div');
 
-        }
-        
+          codeDisplay.classList.add('code-display');
+
+          const string = targetString;
+
+          codeDisplay.innerHTML = string;
+
+          const block1 = document.getElementById('block1');
+
+          if (island) island.appendChild(codeDisplay);
+
+          setTimeout(() => {
+            codeDisplay.remove();
+          }, 1000);
     }
-
   }
-
+}
